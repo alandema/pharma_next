6cbe72663a5a24c1c12b29d515a0a0f2806cdde9
Alan <alann.maman@gmail.com>
2025-11-09 17:49:47 -0300

    fix init migration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,68 @@
+﻿-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "rg" TEXT,
+    "gender" TEXT,
+    "cpf" TEXT,
+    "birth_date" TEXT,
+    "phone" TEXT,
+    "zipcode" TEXT,
+    "street" TEXT,
+    "district" TEXT,
+    "house_number" TEXT,
+    "additional_info" TEXT,
+    "country" TEXT,
+    "state" TEXT,
+    "city" TEXT,
+    "medical_history" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "prescriptions" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "patient_id" INTEGER NOT NULL,
+    "date_prescribed" TEXT NOT NULL,
+    "json_form_info" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "prescriptions_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "patients" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "cids" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "code" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "medications" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "information" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_cpf_key" ON "patients"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cids_code_key" ON "cids"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "medications_name_key" ON "medications"("name");
+
