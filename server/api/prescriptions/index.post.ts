9537510fa75073761379e19551098aae4d5947eb
alandema <alan.maman@gmail.com>
2026-03-26 22:21:29 -0300

    improved template

--- server/api/prescriptions/index.post.ts
+++ server/api/prescriptions/index.post.ts
@@ -92,7 +92,7 @@ export default defineEventHandler(async (event) => {
       throw createError({ statusCode: 400, statusMessage: 'Não foi possível gerar preview para este paciente/prescritor.' });
     }
 
-    const previewBuffer = await generatePDFDocument(formInfo, prescriber.username, patient.name);
+    const previewBuffer = await generatePDFDocument(event, formInfo, prescriber, patient);
     const previewHash = createHash('sha256').update(previewBuffer).digest('hex');
 
     return {
